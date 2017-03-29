36e81335b79dd81776d30117d623a5ed74d6c32f
bowei <bowei.han100@gmail.com>
2017-03-28 22:12:06 -0400

    starting to reimplement sidebar

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -76,26 +76,21 @@ export class ListComponent implements OnInit {
 
     addTask(taskInput, startDateInput, endDateInput, event) {
         let that = this;
-
         if(!(event.keyCode == 13 || event.type === "click")) {
             return;
         }
-
         if(!(taskInput.value && startDateInput.value && endDateInput.value)) {
             bootbox.alert('Please fill out all form fields.');
             return;
         }
-
         if (Date.parse(startDateInput.value) > Date.parse(endDateInput.value)) {
             bootbox.alert("Task start date can't be after the end date!");
             return;
         }
-
         let task = new Task(null, taskInput.value, taskInput.value, startDateInput.value,
                             endDateInput.value, null, parseInt(this.selectedCategoryId),
                             parseInt(localStorage.getItem("user_id")),
                             this.colors[Math.floor(Math.random() * 4)], false);
-
         this.taskService.createTask(task).subscribe(
             task => {
                 that.insertIntoTasksObject(task);
@@ -108,7 +103,6 @@ export class ListComponent implements OnInit {
     insertIntoTasksObject(task) {
         let afterPrevious, beforeNext, numTasks = this.tasks.length;
         task = this.addTagIfOverdue(task);
-
         for (let i = 0; i <= numTasks; i++) {
             if (this.tasks[i]) {
                 afterPrevious = Date.parse(this.tasks[i].end) > Date.parse(task.end);
@@ -125,11 +119,9 @@ export class ListComponent implements OnInit {
     addTagIfOverdue(task) {
         let date = new Date();
         let offsetInMillis = date.getTimezoneOffset()*60000;
-
         if (Date.parse(task.end) < (date.getTime() - offsetInMillis)) {
             task.overdue = true;
         }
-
         return task;
     }
 
@@ -158,26 +150,22 @@ export class ListComponent implements OnInit {
     }
 
     addCategory(categoryInput, event) {
-      let that = this;
-
-      if (!(event.keyCode == 13 || event.type === "click")) {
-          return;
-      }
-
-      if(categoryInput.value) {
-          bootbox.alert('Please enter a category.');
-      }
-
-      let category = new Category(null, categoryInput.value,
-                                  parseInt(localStorage.getItem("user_id")));
-
-      this.categoryService.createCategory(category).subscribe(
-          category => {
-              that.categories.push(category);
-              that.hideCategoryInput(category); // TODO: replace this with in-place edit library
-              categoryInput.value = null;
-          }
-      )
+        let that = this;
+        if (!(event.keyCode == 13 || event.type === "click")) {
+            return;
+        }
+        if(categoryInput.value) {
+            bootbox.alert('Please enter a category.');
+        }
+        let category = new Category(null, categoryInput.value,
+                                    parseInt(localStorage.getItem("user_id")));
+        this.categoryService.createCategory(category).subscribe(
+            category => {
+                that.categories.push(category);
+                that.hideCategoryInput(category); // TODO: replace this with in-place edit library
+                categoryInput.value = null;
+            }
+        )
     }
 
     selectCategory(event, categoryTitle, category) {
@@ -216,84 +204,94 @@ export class ListComponent implements OnInit {
             prev.toggleClass('display-none');
             inputBox.toggleClass('display-inline');
             if (inputBox.hasClass('display-inline')) {
-              inputBox.val(prev.text()).focus();
+                inputBox.val(prev.text()).focus();
             }
         } else if (type === "task") {
-          bootbox.alert('This feature is in development.');
+            bootbox.alert('This feature is in development.');
         }
     }
 
-    updateCategory(event, category_id) { // refactor this with update task
-      if(event.keyCode == 13) {
+    updateCategory(event, categoryId) { // refactor this with update task
+        if(event.keyCode != 13) {
+            return;
+        }
         if($(event.currentTarget).val()) {
-          let params = { 'name':$(event.currentTarget).val() }
-          this.categoryService.updateCategory(category_id, params); // doesn't need to wait for server response
-          this.updateCategorySuperficially(category_id, $(event.currentTarget).val());
-          this.toggleEdit(category_id, 'category');
+            let params = {
+                'name':$(event.currentTarget).val()
+            };
+            this.categoryService.updateCategory(categoryId, params); // doesn't need to wait for server response
+            this.updateCategorySuperficially(categoryId, $(event.currentTarget).val());
+            this.toggleEdit(categoryId, 'category');
         } else {
-          bootbox.alert('Category name must not be empty');
+            bootbox.alert('Category name must not be empty');
         }
-      }
     }
 
-    updateTask(event, task_id) {
-      if(event.keyCode == 13) {
+    updateTask(event, taskId) {
+        if(event.keyCode != 13) {
+            return;
+        }
         if($(event.currentTarget).val()) {
-          let params = { 'title':$(event.currentTarget).val() }
-          this.taskService.updateTask(task_id, params); // doesn't need to wait for server response
-          this.updateTaskSuperficially(task_id, $(event.currentTarget).val());
-          this.toggleEdit(task_id, 'task');
+            let params = {
+                'title':$(event.currentTarget).val()
+            };
+            this.taskService.updateTask(taskId, params); // doesn't need to wait for server response
+            this.updateTaskSuperficially(taskId, $(event.currentTarget).val());
+            this.toggleEdit(taskId, 'task');
         } else {
-          bootbox.alert('Task title must not be empty');
+            bootbox.alert('Task title must not be empty');
         }
-      }
     }
 
-    updateTaskSuperficially(task_id, title) {
-      for (let i = 0; i < this.tasks.length; i++) {
-        if (this.tasks[i].id === task_id) {
-          this.tasks[i].title = title; return;
+    updateTaskSuperficially(taskId, title) {
+        for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i].id === taskId) {
+                this.tasks[i].title = title;
+                return;
+            }
         }
-      }
     }
 
-    updateCategorySuperficially(category_id, name) {
-      for (let i = 0; i < this.categories.length; i++) {
-        if (this.categories[i].id === category_id) {
-          this.categories[i].name = name; return;
+    updateCategorySuperficially(categoryId, name) {
+        for (let i = 0; i < this.categories.length; i++) {
+            if (this.categories[i].id === categoryId) {
+                this.categories[i].name = name;
+                return;
+            }
         }
-      }
     }
 
     deleteCategory(category, event) {
-      let that = this;
-      event.stopPropagation();
-      bootbox.confirm("Are you sure you want to delete category: " + category.name + "?", function(response) {
-        if (response) {
-          if (category.id === that.selectedCategoryId) { that.selectedCategoryId = null; } // if you delete current category
-          that.categoryService.deleteCategory(category.id).subscribe(
-              callback => {
-                  for (let i = 0; i < that.categories.length; i++) {
-                      if (that.categories[i] === category) {
-                          that.categories.splice(i, 1);
-                      }
-                  }
-                  that.fetchTasks(false);
-              }
-          )
-        }
-      });
+        let that = this;
+        event.stopPropagation();
+        bootbox.confirm("Delete category: " + category.name + "?", function(response) {
+            if (response) {
+                if (category.id === that.selectedCategoryId) {
+                    that.selectedCategoryId = null;
+                }
+                that.categoryService.deleteCategory(category.id).subscribe(
+                    callback => {
+                        for (let i = 0; i < that.categories.length; i++) {
+                            if (that.categories[i] === category) {
+                                that.categories.splice(i, 1);
+                            }
+                        }
+                        that.fetchTasks(false);
+                    }
+                )
+            }
+        });
     }
 
     showCategoryInput() {
-      $('.js-category-label').hide();
-      $('.js-category-input').show();
-      $('input.js-category-input').focus();
+        $('.js-category-label').hide();
+        $('.js-category-input').show();
+        $('input.js-category-input').focus();
     }
 
     hideCategoryInput(categoryInput) {
-      categoryInput.value = null;
-      $('.js-category-input').hide();
-      $('.js-category-label').show();
+        categoryInput.value = null;
+        $('.js-category-input').hide();
+        $('.js-category-label').show();
     }
 }
--- src/app/components/sidebar (currently deprecated)/sidebar.component.ts
+++ src/app/components/sidebar (currently deprecated)/sidebar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category-service/category.service';
+import { TaskService } from '../../services/task-service/task.service';
+import { CategoryService } from '../../services/category-service/category.service';
+import { Colors } from '../../shared/app-colors';
 
 declare var $: any;
 
@@ -12,42 +15,4 @@ declare var $: any;
 })
 export class SidebarComponent implements OnInit {
 
-  categories = [];
-
-  constructor(
-    private categoryService: CategoryService
-  ) { }
-
-  ngOnInit() {
-    this.initializeCategories();
-  }
-
-  initializeCategories() {
-    var that = this;
-    $.when(this.categoryService.getCategoriesForUser(localStorage.getItem("user_id"))).done(function (response) {
-      let categories = JSON.parse(response._body);
-      for (var i = 0; i < categories.length; i++) {
-        that.categories.push(categories[i]);
-      }
-    });
-  }
-
-  addCategory(categoryInput, event) {
-    if(event.keyCode == 13 || event.type === "click") {
-      if(categoryInput.value) {
-        let category = new Category(null, categoryInput.value, parseInt(localStorage.getItem("user_id")));
-        this.categoryService.createCategory(category);
-        this.categories.push(category);
-        categoryInput.value = null;
-      } else {
-        alert('Please enter a category');
-      }
-    }
-  }
-
-  selectCategory(event) {
-    $.when(this.categoryService.getCategoryByName(event.currentTarget.innerText)).done(function (response) {
-      localStorage.setItem("selectedCategoryId", JSON.parse(response._body).id);
-    });
-  }
 }
