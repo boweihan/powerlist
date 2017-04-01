9418e2622200e8c280d388d59ec102b4ef44ffb5
bowei <bowei.han100@gmail.com>
2017-04-01 18:51:56 -0400

    added graph template

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let echarts: any;
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
@@ -10,6 +11,115 @@ export class GraphComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      let that = this;
+      let myChart = echarts.init(document.getElementById('graph'));
+      this.initChart(myChart);
+      window.addEventListener('resize', myChart.resize, false);
+  }
+
+  initChart(myChart) {
+
+      // map out data and date here, using number of tasks per day
+      let base = +new Date(1968, 9, 3);
+      let oneDay = 24 * 3600 * 1000;
+      let date = [];
+
+      let data = [Math.random() * 300];
+
+      for (let i = 1; i < 20000; i++) {
+          let now = new Date(base += oneDay);
+          date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
+          data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
+      }
+
+      let option = {
+          tooltip: {
+              trigger: 'axis',
+              position: function (pt) {
+                  return [pt[0], '10%'];
+              }
+          },
+          title: {
+              left: 'center',
+              text: 'PowerList Work Balancer',
+          },
+          toolbox: {
+              feature: {
+                  dataZoom: {
+                      title: {
+                          zoom: 'enable zoom',
+                          back: 'zoom out'
+                      },
+                      yAxisIndex: 'none'
+                  },
+                  restore: {
+                      title: 'refresh'
+                  },
+                  saveAsImage: {
+                      title: 'download'
+                  }
+              },
+              right: '10%'
+          },
+          xAxis: {
+              type: 'category',
+              boundaryGap: false,
+              data: date
+          },
+          yAxis: {
+              type: 'value',
+              boundaryGap: [0, '100%'],
+              name: 'number of tasks',
+              nameRotate: 90,
+              nameLocation: 'middle'
+          },
+          dataZoom: [{
+              type: 'inside',
+              start: 0,
+              end: 10
+          }, {
+              start: 0,
+              end: 10,
+              handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+              handleSize: '80%',
+              handleStyle: {
+                  color: '#fff',
+                  shadowBlur: 3,
+                  shadowColor: 'rgba(0, 0, 0, 0.6)',
+                  shadowOffsetX: 2,
+                  shadowOffsetY: 2
+              }
+          }],
+          series: [
+              {
+                  name:'Data',
+                  type:'line',
+                  smooth:true,
+                  symbol: 'none',
+                  sampling: 'average',
+                  itemStyle: {
+                      normal: {
+                          color: 'rgb(255, 70, 131)'
+                      }
+                  },
+                  areaStyle: {
+                      normal: {
+                          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                              offset: 0,
+                              color: 'rgb(255, 158, 68)'
+                          }, {
+                              offset: 1,
+                              color: 'rgb(255, 70, 131)'
+                          }])
+                      }
+                  },
+                  data: data
+              }
+          ]
+      };
+
+      myChart.setOption(option);
+
   }
 
 }
